bbe2fdbedbd2ee260c7d66475233b16babb76ec2
magdyZiad <magdyziad.dev@gmail.com>
2025-12-27 11:38:45 +0200

    add some features to icon

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const mode = document.getElementById("mode");
+const nav = document.getElementById("nav");
+const icon = document.getElementById("icon");
+
+if (window.localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("mode");
+  nav.classList.add("mode");
+  icon.classList.remove("bi-moon-fill");
+  icon.classList.add("bi-sun-fill");
+}
 
 mode.onclick = (e) => {
   document.body.classList.toggle("mode");
-  document.getElementById("nav").classList.toggle("mode");
+  nav.classList.toggle("mode");
   if (e.target.classList.contains("bi-moon-fill")) {
     e.target.classList.remove("bi-moon-fill");
     e.target.classList.add("bi-sun-fill");
   } else {
     e.target.classList.remove("bi-sun-fill");
     e.target.classList.add("bi-moon-fill");
+  }
+
+  if (document.body.classList.contains("mode")) {
+    window.localStorage.setItem("theme", "dark");
+  } else {
+    window.localStorage.setItem("theme", "light");
   }
 };
